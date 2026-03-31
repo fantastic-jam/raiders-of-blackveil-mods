f33d49f2e5a8b06f1dbd41b2124fc599681ddb8c
christphe <christphe@ffxmail.com>
2026-03-31 23:44:56 +0200

    chore(HandyPurse): release v0.0.2

--- HandyPurse/HandyPurseMod.cs
+++ HandyPurse/HandyPurseMod.cs
@@ -13,7 +13,7 @@ namespace HandyPurse
     {
         private const string Id = "io.github.fantastic-jam.raidersofblackveil.mods.handypurse";
         public const string Name = "HandyPurse";
-        public const string Version = "0.0.1";
+        public const string Version = "0.0.2";
         public const string Author = "christphe";
 
         public static ManualLogSource PublicLogger;
@@ -50,4 +50,5 @@ namespace HandyPurse
         }
     }
 }
+
 
