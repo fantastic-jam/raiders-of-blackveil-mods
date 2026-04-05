3ee5080faa0cbcf866771da6b9e0200de72e1db3
christphe <christphe@ffxmail.com>
2026-04-05 12:17:34 +0200

    chore(PerfectDodge): release v0.3.0

--- mods/PerfectDodge/PerfectDodgeMod.cs
+++ mods/PerfectDodge/PerfectDodgeMod.cs
@@ -11,7 +11,7 @@ namespace PerfectDodge {
     public class PerfectDodgeMod : BaseUnityPlugin, IModRegistrant {
         private const string Id = "io.github.fantastic-jam.raidersofblackveil.mods.perfectdodge";
         public const string Name = "PerfectDodge";
-        public const string Version = "0.2.0";
+        public const string Version = "0.3.0";
         public const string Author = "christphe";
 
         public static ManualLogSource PublicLogger;
