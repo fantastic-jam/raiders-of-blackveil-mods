3faf6eedfbcfb73a2487130f908d26db274fc1db
christphe <christphe@ffxmail.com>
2026-04-06 05:12:37 +0200

    chore(DisableSkillsBar): release v0.5.2

--- mods/DisableSkillsBar/DisableSkillsBarMod.cs
+++ mods/DisableSkillsBar/DisableSkillsBarMod.cs
@@ -9,7 +9,7 @@ namespace DisableSkillsBar {
     public class DisableSkillsBarMod : BaseUnityPlugin {
         private const string Id = "io.github.fantastic-jam.raidersofblackveil.mods.disableskillsbar";
         public const string Name = "DisableSkillsBar";
-        public const string Version = "0.5.1";
+        public const string Version = "0.5.2";
         public const string Author = "christphe";
 
         public static ManualLogSource PublicLogger;
